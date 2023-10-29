83f647ffd38ca631176f801bed1ce32c73207c91
Ju4nk4S <102430751+Ju4nk4S@users.noreply.github.com>
2023-10-29 03:46:12 +0000

    1.0

---
+++ MiEstacionamientoApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent, ], imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    HttpClientModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,    
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirebaseApp(() => initializeApp({"projectId":"prueba-bbd7a","appId":"1:1093424109059:web:a05fa865efe3d54086e036","storageBucket":"prueba-bbd7a.appspot.com","apiKey":"AIzaSyCni8DNeJr-0vdRXEspvBct1WASq9VyYe8","authDomain":"prueba-bbd7a.firebaseapp.com","messagingSenderId":"1093424109059","measurementId":"G-7ZE0H5LWQ5"})),
+    provideFirestore(() => getFirestore())
+    
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
